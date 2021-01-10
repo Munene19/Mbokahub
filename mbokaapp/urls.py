e18bbc499d3a1ee116be8a4a4bb7b1d2891dbb3a
Mike Munene <you@example.com>
2021-01-11 00:10:53 +0300

    job urls created

--- mbokaapp/urls.py
+++ mbokaapp/urls.py
@@ -4,5 +4,9 @@ from mbokaapp import views
 app_name = "mbokaapp"
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('jobs/', views.job_list_View, name='job-list'),
+    path('job/create/', views.create_job_View, name='create-job'),
+    path('job/<int:id>/', views.single_job_view, name='single-job'),
 
 ]
