cac65637c31dc3ed1e8bc86afcdfae6cc3d63a69
Mike Munene <you@example.com>
2021-01-10 20:08:09 +0300

    categorym, job and applicants models created

--- account/managers.py
+++ account/managers.py
@@ -4,15 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
-
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
         if not email:
             raise ValueError('The Email must be set')
         if not password:
@@ -24,9 +16,6 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
