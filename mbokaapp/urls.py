666eb3784ff195d56b22b4b3d114c680d7c4bce8
Mike Munene <you@example.com>
2021-01-11 00:12:09 +0300

    bookmarkjob url added

--- mbokaapp/urls.py
+++ mbokaapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('job/create/', views.create_job_View, name='create-job'),
     path('job/<int:id>/', views.single_job_view, name='single-job'),
     path('apply-job/<int:id>/', views.apply_job_view, name='apply-job'),
+    path('bookmark-job/<int:id>/', views.job_bookmark_view, name='bookmark-job'),
+
 
 
 ]
